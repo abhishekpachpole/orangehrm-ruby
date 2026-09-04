1aa56cbd6d007ac8783da30a5e5ebcd694e7a02e
abhishekpachpole <84066718+abhishekpachpole@users.noreply.github.com>
2026-09-04 11:49:47 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Capybara.app_host = "https://opensource-demo.orangehrmlive.com"
 
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  # options.add_argument("--headless=new")
+  options.add_argument("--headless=new")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--disable-gpu")
